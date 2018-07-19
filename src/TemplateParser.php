e2d99ce7d65a3d361cc53300aa652eaf2a057ab8
Wallace <wallace.souza@grupotmt.com.br>
2018-07-19 13:57:09 -0300

    fix identation

--- src/TemplateParser.php
+++ src/TemplateParser.php
@@ -32,8 +32,8 @@ class TemplateParser
      */
 
     public function getDomDocument()
-	{	
-		return $this->domDocument;
+    {   
+        return $this->domDocument;
     }
     
     /**
@@ -42,58 +42,58 @@ class TemplateParser
      * @param \DomNamedNodeMap $attrs
      * @param \DomElement $node
      */
-	protected function processAttributes(\DOMNamedNodeMap $attrs, \DomElement $node)
-	{
-		foreach($attrs as $attr) {
+    protected function processAttributes(\DOMNamedNodeMap $attrs, \DomElement $node)
+    {
+        foreach($attrs as $attr) {
 
-			if (strpos($attr->name, static::SPECIAL_ATTRIBUTE_PREFIX) !== 0) continue;
-			
+            if (strpos($attr->name, static::SPECIAL_ATTRIBUTE_PREFIX) !== 0) continue;
+            
             $name = substr($attr->name, strlen(static::SPECIAL_ATTRIBUTE_PREFIX));
             
             $method = 'parseAttribute' . ucfirst($name);
             
-			if (! method_exists($this, $method)) {
-				throw new \RuntimeException("Method '{$name}' not yet implemented");
+            if (! method_exists($this, $method)) {
+                throw new \RuntimeException("Method '{$name}' not yet implemented");
             }
             
             $node->removeAttribute($attr->name);
             
-			$this->$method($node, $attr);
-			
-		}
+            $this->$method($node, $attr);
+            
+        }
     }
     
     /**
      * 
      * 
      */
-	protected function recursiveNodeProcess(\DomElement $nodes)
-	{
-		
-		foreach ($nodes->childNodes as $node) {
-			$node->hasChildNodes() && $this->recursiveNodeProcess($node);
-			if ($node->nodeType === XML_ELEMENT_NODE){
-				$this->processAttributes($node->attributes, $node);
-			}
-		}
+    protected function recursiveNodeProcess(\DomElement $nodes)
+    {
+        
+        foreach ($nodes->childNodes as $node) {
+            $node->hasChildNodes() && $this->recursiveNodeProcess($node);
+            if ($node->nodeType === XML_ELEMENT_NODE){
+                $this->processAttributes($node->attributes, $node);
+            }
+        }
     }
     
 
     /**
-     * Transform bind expression in "echo" sintaxe 			
+     * Transform bind expression in "echo" sintaxe          
      * 
      * @param \Domelement $node
      * @param \DomAttr $attr
      * @return void
      */
-	protected function parseAttributeBind(\DomElement $node, \DomAttr $attr)
-	{
-		$node->removeAttribute(static::SPECIAL_ATTRIBUTE_PREFIX . 'bind');
-		$node->nodeValue = ''; // remove all childs!
-		$expression = sprintf('<?= %s; ?>', $this->normalizePHPExpression($attr->value));
-		$php = $this->getDomDocument()->createCDATASection($expression);
-		$node->appendChild($php);
-		
+    protected function parseAttributeBind(\DomElement $node, \DomAttr $attr)
+    {
+        $node->removeAttribute(static::SPECIAL_ATTRIBUTE_PREFIX . 'bind');
+        $node->nodeValue = ''; // remove all childs!
+        $expression = sprintf('<?= %s; ?>', $this->normalizePHPExpression($attr->value));
+        $php = $this->getDomDocument()->createCDATASection($expression);
+        $node->appendChild($php);
+        
     }
     
     /**
@@ -103,31 +103,31 @@ class TemplateParser
      * @param \DomAttr $attr
      * @return void
      */
-	protected function parseAttributeForeach(\DomElement $node, \DomAttr $attr)
-	{
+    protected function parseAttributeForeach(\DomElement $node, \DomAttr $attr)
+    {
         $dom = $this->getDomDocument();
         
-		$expressionStart = sprintf('<?php foreach(%s): ?>', $this->normalizePHPExpression($attr->value));
-		$expressionEnd = '<?php endforeach ?>';
-	
-		$node->insertBefore($dom->createCDATASection($expressionStart), $node->firstChild);
-		$node->appendChild($dom->createCDATASection($expressionEnd));
-		
+        $expressionStart = sprintf('<?php foreach(%s): ?>', $this->normalizePHPExpression($attr->value));
+        $expressionEnd = '<?php endforeach ?>';
+        
+        $node->insertBefore($dom->createCDATASection($expressionStart), $node->firstChild);
+        $node->appendChild($dom->createCDATASection($expressionEnd));
+        
     }
     
-	protected function normalizePHPExpression($string)
-	{
-		return rtrim($string, ';');
+    protected function normalizePHPExpression($string)
+    {
+        return rtrim($string, ';');
     }
 
 
-	public function parse()
-	{
+    public function parse()
+    {
         $dom = $this->getDomDocument();
         
-		$this->recursiveNodeProcess($dom->documentElement);
+        $this->recursiveNodeProcess($dom->documentElement);
         
-		return $dom->saveHtml();
-		
-	}
+        return $dom->saveHtml();
+        
+    }
 }
